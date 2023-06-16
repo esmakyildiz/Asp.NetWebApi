10a6f5a4988b457e8df8d6f1eb3cd0823df25cfa
EsmaNur Akyıldız <EsmaNur Akyldz@LAPTOP-NJL3HTBN>
2023-06-17 00:59:40 +0300

    Add project files.

---
+++ Asp.NetWebApi/Controllers/PersonelController.cs
@@ -0,0 +1,45 @@
+﻿using Asp.NetWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Asp.NetWebApi.Controllers
+{
+    public class PersonelController : ApiController
+    {
+        private List<Personel> listPersoneller =new List<Personel>
+        
+        {
+            new Personel{Id=1, Ad="Ali", Soyad="Hazır"},
+            new Personel{Id=2, Ad="Osman", Soyad="Hazır"},
+            new Personel{Id=3, Ad="Toprak", Soyad="Erzurumluoğlu"}
+        };
+
+        public IEnumerable<Personel> GetPersoneller()
+        {
+            return listPersoneller;
+        }
+        public IHttpActionResult GetPersonel(int id)
+        {
+            var arananPersonel = (from p in listPersoneller
+                                  where p.Id == id
+                                  select p).FirstOrDefault();
+            return Ok(arananPersonel);
+        }
+
+        public IHttpActionResult PostPersonel(Personel per)
+        {
+            if(listPersoneller.Where(p => p.Id == per.Id).Count() == 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return Conflict();
+            }
+        }
+    }
+}
---
+++ Asp.NetWebApi/Controllers/PersonellerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.NetWebApi.Controllers
+{
+    public class PersonellerController : Controller
+    {
+        // GET: Personeller
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TumPersoneller()
+        {
+            return View();
+        }
+        public ActionResult PersonelGetir()
+        {
+            return View();
+        }
+
+        public ActionResult YeniPersoneller()
+        {
+            return View();
+        }
+    }
+
+}
